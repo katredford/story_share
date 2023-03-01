4113dc074b7425aad9dea869d6bac2a0ce0ba1d5
katredford <katharine.redford@gmail.com>
2023-03-01 13:57:44 -0600

    all crud operations for user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -11,7 +11,25 @@ router.get("/", async (req, res) => {
   }
 });
 
- 
+ // GET a single user
+router.get("/:id", async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.params.id, {
+      // JOIN with locations, using the Trip through table
+      // include: [{ model: Location, through: Trip, as: "planned_trips" }],
+    });
+
+    if (!userData) {
+      res.status(404).json({ message: "No user found with this id!" });
+      return;
+    }
+
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 
 // CREATE a user
 router.post("/", async (req, res) => {
@@ -23,5 +41,38 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.put("/:id", (req, res) => {
+  User.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((user) => {
+
+      res.status(200).json({ user: req.body })
+    })
+    .catch((err) => res.status(400).json(err));
+});
+
+
+// DELETE a user
+router.delete("/:id", async (req, res) => {
+  try {
+    const userData = await User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!userData) {
+      res.status(404).json({ message: "No user found with this id!" });
+      return;
+    }
+
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
